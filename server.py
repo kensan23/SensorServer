6b12c28381fda17e71c9d22f1a30648cd72a17fb
kensan <9150185+kensan23@users.noreply.github.com>
2019-09-16 20:31:09 +1000

    clean up endpoints
    clean up endpoints
    add getall endpoint

--- server.py
+++ server.py
@@ -2,30 +2,41 @@
 from flask import Flask, Response
 from flask_restful import Api
 import Adafruit_DHT
+import json
 
 application = Flask(__name__)
-api = Api(app)
+api = Api(application)
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
-@app.route("/humidity", methods=['GET']) 
+@application.route("/humidity", methods=['GET']) 
 def humidity():
-        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-        if humidity is not None and temperature is not None:
+        humidity = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)[0]
+        if humidity is not None:
                 return Response("{0:.2f}".format(humidity), status=200, mimetype='text/plain')
         else:
-                return "Failed to retrieve data from humidity sensor", 503
-@app.route("/temperature", methods=['GET']) 
+                return "Failed to retrieve data from dht22 sensor", 503
+@application.route("/temperature", methods=['GET']) 
 def temperature():
-        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-        if humidity is not None and temperature is not None:
+        temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)[1]
+        if temperature is not None:
                 return Response("{0:0.1f}".format(temperature), status=200, mimetype='text/plain')
         else:
-                return "Failed to retrieve data from temperature sensor", 503
+                return "Failed to retrieve data dht22 sensor", 503
+
+@application.route("/all", methods=['GET']) 
+def getAll():
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        response = dict({"humidity":"{0:.2f}".format(humidity), "temperature":"{0:0.1f}".format(temperature)})
+
+        if humidity is not None and temperature is not None:
+                return Response(json.dumps(response), status=200, mimetype='application/json')
+        else:
+                return "Failed to retrieve data from dht22", 503
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0')
+        application.run(host='0.0.0.0')
 
 
 
